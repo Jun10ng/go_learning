018acbc82960e48a6b57f108edbdfb0475479729
Jun10ng <fu4ng@foxmail.com>
2020-09-21 23:52:30 +0800

    update

--- solutions/leetcode/editor/test/main.go
+++ solutions/leetcode/editor/test/main.go
@@ -1,47 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
-func main()  {
-	fmt.Print(partition("aba"))
+func main() {
+	fmt.Print(isAdditiveNumber("1235813"))
 	//fmt.Print(ok("aabaa"))
 }
-func partition(s string) [][]string {
-	ans := [][]string{}
-	path := []string{}
-	trace := func(path []string, idx int) {}
+func isAdditiveNumber(num string) bool {
+	ret := false
+	//l,ml := len(num),len(num)/2 //单个数字长度不大于ml
+	trace := func(cnt, n1, n2 int, rest string) {}
+	// cnt 是目前池内数字，当cnt > 2 才会判断
+	// rest 是可选择的string
 
-	trace = func(path []string,idx int) {
-		if idx == len(s){
-			cp := make([]string,len(path))
-			copy(cp,path)
-			ans = append(ans,cp)
+	trace = func(cnt, n1, n2 int, rest string) {
+		if len(rest) == 0 {
+			if cnt > 2 {
+				ret = true
+			}
 			return
 		}
-		for i:= idx;i<len() {
-			
-		}
-	}
-	trace(path,0)
-	return ans
-}
 
-// 判断是否为回文串
-func listOk(path []string) bool  {
-	for _,e :=range path{
-		if !ok(e) {
-			return false
+		//开始从rest中取值，添加分支并剪枝
+		n3 := ""
+		for _, e := range rest {
+			n3 = n3 + string(e)
+			// 0 可以新建分支 01 02 则剪枝
+			if n3[0] == '0' && len(n3) != 1 {
+				return
+			}
+			e, _ := strconv.Atoi(n3)
+			if cnt >= 2 {
+				if e == n1+n2 {
+					trace(cnt+1, n2, e, rest[len(n3):])
+				}
+			} else {
+				trace(cnt+1, n2, e, rest[len(n3):])
+			}
+			if ret {
+				return
+			}
 		}
 	}
-	return true
-}
 
-func ok(p string) bool {
-	l := len(p)-1
-	for i:=0;i<=l/2;i++ {
-		if p[i]!=p[l-i] {
-			return false
-		}
-	}
-	return true
+	//199100199
+	//
+	trace(0, 0, 0, num)
+	return ret
 }
