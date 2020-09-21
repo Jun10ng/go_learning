44b62f2413c4f8ad9ce5adc022af8ab67f3577e1
Jun10ng <fu4ng@foxmail.com>
2020-09-21 22:33:56 +0800

    update

--- solutions/leetcode/editor/test/main.go
+++ solutions/leetcode/editor/test/main.go
@@ -1,90 +1,47 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-//给定二叉树: [3,9,20,null,null,15,7],
-//
-//     3
-//   / \
-//  9  20
-//    /  \
-//   15   7
-//
-//
-// 返回其层次遍历结果：
-//
-// [
-//  [3],
-//  [20,9],
-//  [15,7]
-//]
-//
-
-//* Definition for a binary tree node.
-func main() {
-	fmt.Print(solveNQueens(4))
+func main()  {
+	fmt.Print(partition("aba"))
+	//fmt.Print(ok("aabaa"))
 }
-func solveNQueens(n int) [][]string {
-	ans := &[][]string{}
-	cs := &[]string{}
+func partition(s string) [][]string {
+	ans := [][]string{}
+	path := []string{}
+	trace := func(path []string, idx int) {}
 
-	var line string
-	for i := 0; i < n; i++ {
-		line += "."
-	}
-	for i := 0; i < n; i++ {
-		*cs = append(*cs, line)
-	}
-
-	//
-	trace(ans, cs, 0, 0, n)
-	return *ans
-}
-
-func trace(ans *[][]string, cs *[]string, row, col, n int) {
-	if row == n {
-		cp := make([]string, n)
-		copy(cp, *cs)
-		*ans = append(*ans, cp)
-		return
-	}
-
-	for i := 0; i < n; i++ {
-		if !Ok(cs, row, i) {
-			continue
+	trace = func(path []string,idx int) {
+		if idx == len(s){
+			cp := make([]string,len(path))
+			copy(cp,path)
+			ans = append(ans,cp)
+			return
 		}
-		(*cs)[row] = (*cs)[row][:i] + "Q" + (*cs)[row][i+1:]
-		trace(ans, cs, row+1, 0, n)
-		(*cs)[row] = (*cs)[row][:i] + "." + (*cs)[row][i+1:]
+		for i:= idx;i<len() {
+			
+		}
 	}
-
+	trace(path,0)
+	return ans
 }
 
-func Ok(cs *[]string, row, col int) bool {
-	n := len(*cs)
-	for i := 0; i < n; i++ {
-		if (*cs)[i][col] == 'Q' {
+// 判断是否为回文串
+func listOk(path []string) bool  {
+	for _,e :=range path{
+		if !ok(e) {
 			return false
 		}
 	}
-	//左上
-	for i, j := row-1, col-1; i >= 0 && j >= 0; {
-		if (*cs)[i][j] == 'Q' {
-			return false
-		}
-		i--
-		j--
-	}
-	//右上
-	for i, j := row-1, col+1; i >= 0 && j < n; {
-		if (*cs)[i][j] == 'Q' {
-			return false
-		}
-		i--
-		j++
-	}
+	return true
+}
 
+func ok(p string) bool {
+	l := len(p)-1
+	for i:=0;i<=l/2;i++ {
+		if p[i]!=p[l-i] {
+			return false
+		}
+	}
 	return true
 }
