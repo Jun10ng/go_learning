a936ddf41a95f4060b64f02ee31dad650ad0457a
Jun10ng <fu4ng@foxmail.com>
2020-10-02 17:35:44 +0800

    update

---
+++ solutions/leetcode/editor/cn/[1079]活字印刷.go
@@ -0,0 +1,40 @@
+package main
+//你有一套活字字模 tiles，其中每个字模上都刻有一个字母 tiles[i]。返回你可以印出的非空字母序列的数目。 
+//
+// 注意：本题中，每个活字字模只能使用一次。 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入："AAB"
+//输出：8
+//解释：可能的序列为 "A", "B", "AA", "AB", "BA", "AAB", "ABA", "BAA"。
+// 
+//
+// 示例 2： 
+//
+// 输入："AAABBC"
+//输出：188
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= tiles.length <= 7 
+// tiles 由大写英文字母组成 
+// 
+// Related Topics 回溯算法 
+// 👍 84 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func numTilePossibilities(tiles string) int {
+	if len(tiles) == 1 {
+		return 1
+	}
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
