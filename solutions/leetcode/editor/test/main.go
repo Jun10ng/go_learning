c1f2dad624d74a15854e7b68989c13aaea86036f
Jun10ng <fu4ng@foxmail.com>
2021-03-02 00:02:03 +0800

    update

--- solutions/leetcode/editor/test/main.go
+++ solutions/leetcode/editor/test/main.go
@@ -5,49 +5,39 @@ import (
 )
 
 func main() {
-	fmt.Println(countSubstrings("aaaaa"))
-}
-func countSubstrings(s string) int {
-	/*
-			dp[i][j] : s[i~j]是否为回文
-			dp[i][j] = s[i] == s[j] && dp[i+1][j-1]
-			dp[0][0] = 0
+	fmt.Println(minSwap([]int{1, 3, 5, 4}, []int{1, 2, 3, 7}))
+	// 1 3 10 7 12
+	// 1 2 6 11 9
 
-		 	0 0 0 0 0 0
-		    0 1 0 0 0 0
-			0 0 1 0 0 0
-			0 0 0 1 1 1
-			0 0 0 0 1 1
-		0 0 0 0 0 1
+}
+func minSwap(A []int, B []int) int {
+	/*
+		第0状态 不交换
+		第1状态 交换
+		dp[i][j] 第i个元素，在第j状态下的最小交换次数
+		dp[0][0] = 0 dp[0][1] = 1
+
+		第i个元素换不换 取决于 i-1个元素是否有序
+		状态转移方程
+		dp[i][j] =
+		A[i-1] < A[i] && B[i-1]<B[i]
+			A[i-1] < B[i] && B[i-1] < A[i] //取最优值
+				dp[i][0] = min(dp[i-1][0],dp[i-1][1]) //不交换
+				dp[i][1] = min(dp[i-1][0],dp[i-1][1]) + 1 // 交换
+			else // 这种情况一旦交换了 i元素 i-1元素也得交换，才能保持严格递增
+				dp[i][0] = dp[i-1][0]
+				dp[i][1] = dp[i-1][1] + 1
+		else // 需要交换
+			dp[i][0] = dp[i-1][1] //不交换，则上个位置必须交换
+			dp[i][1] = dp[i-1][0]+1          //交换，则上位置不交换
+
+		  0 0
+		0 0 1
+		0
+		0
+		0
 	*/
-	if len(s) == 0 || len(s) == 1 {
-		return len(s)
-	}
-	dp := make([][]bool, 0)
-	for i := 0; i <= len(s); i++ {
-		dpi := make([]bool, len(s)+1)
-		dp = append(dp, dpi)
-	}
-	ret := 0
-	ret += len(s)
-	for i := 1; i <= len(s); i++ {
-		dp[i][i] = true
-	}
-	for i := len(s); i >= 1; i-- {
-		for j := i + 1; j <= len(s); j++ {
-			if s[i-1] == s[j-1] {
-				if j-i == 1 {
-					dp[i][j] = true
-				} else {
-					dp[i][j] = dp[i+1][j-1]
-				}
-				if dp[i][j] {
-					ret++
-				}
-			}
-		}
-	}
-	return ret
+
 }
 func sumOfSlice(s []int, i, j int) int {
 	sum := s[j]
