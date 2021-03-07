57d9e78cef713513b0daee70be8d4a7a0ee61b8e
Jun10ng <fu4ng@foxmail.com>
2021-03-07 11:45:40 +0800

    update

--- solutions/leetcode/editor/test/main.go
+++ solutions/leetcode/editor/test/main.go
@@ -5,38 +5,26 @@ import (
 )
 
 func main() {
-	fmt.Println(distinctSubseqII("lee"))
-	fmt.Println(distinctSubseqII("abc"))
-	/*
-		a b       b
-		1 1+2     3+(4-3)
-	*/
-	//fmt.Println(distinctSubseqII("aaa"))
+	fmt.Println(constrainedSubsetSum([]int{10, -2, -10, -5, 20}, 2))
+	// 输入：nums = [10,-2,-10,-5,20], k = 2
+	//输出：23
+	//解释：子序列为 [10, -2, -5, 20] 。
 }
-func distinctSubseqII(S string) int {
-	/*
-		 // dp[i] 表示 S[0..i) 中有多少个**包含空序列**的不重复子序列
-			1 + 2 + 4 + 8
-
-			a b         a
-			2 4     	6
-			a a         a
-			2 4-2   	4
-	*/
-	//int MOD = 1_000_000_007;
-	N := len(S)
-	dp := make([]int, N+1)
-	dp[0] = 0
-
-	last := make(map[uint8]int)
-
-	for i := 0; i < N; i++ {
-		x := S[i]
-		t := last[x]
-		dp[i+1] = dp[i]*2 + 1 - t
-		last[x] = dp[i+1] - dp[i]
+func constrainedSubsetSum(nums []int, k int) int {
+	dp := make([]int, len(nums)+1)
+	ret := 0
+	for i := 1; i <= len(nums); i++ {
+		dp[i] = nums[i-1]
+		for j := i - 1; i-j <= k && j > 0; j-- {
+			dp[i] = max(dp[i], nums[i-1]+dp[j])
+		}
+		ret = max(ret, dp[i])
 	}
-	//dp[N]--
-	//if (dp[N] < 0) dp[N] += MOD;
-	return dp[N]
+	return ret
+}
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
