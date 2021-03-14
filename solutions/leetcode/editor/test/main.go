49949cda6cd268486034fd62b9784a945dae9855
Jun10ng <fu4ng@foxmail.com>
2021-03-14 15:57:35 +0800

    update

--- solutions/leetcode/editor/test/main.go
+++ solutions/leetcode/editor/test/main.go
@@ -1,56 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	nums := []int{1, -1}
-	fmt.Print(maxSlidingWindow(nums, 1))
-	fmt.Print(maxSlidingWindow([]int{1, 3, -1, -3, 5, 3, 6, 7}, 3))
-	fmt.Print(maxSlidingWindow([]int{3, -2, -3, -1, 5, 3, 6, 7}, 3))
-}
 
-func maxSlidingWindow(nums []int, k int) []int {
-	// 维护一个双端队列，滑动窗口内最大的元素始终是最左边的元素
-	// 向右滑动时，如果push进了一个新的最大值，就把新最大值左端的元素全部出队
-	// 向右滑动时，如果push进了一个非最大值，则把小于该值的元素都出队
-	ret := make([]int, 0)
-	d := &deque{
-		[]int{},
+	fmt.Print(maxProfit([]int{7, 1, 5, 3, 6, 4}))
+}
+func maxProfit(prices []int) int {
+	/*
+		0 -6 4 -2 3 -2
+	*/
+	ret := 0
+	pre := []int{}
+	pre = append(pre, 0)
+	for i := 1; i < len(prices); i++ {
+		pre = append(pre, prices[i]-prices[i-1])
 	}
-	for i, e := range nums {
-		if d.len() >= k {
-			d.pop()
+	for _, e := range pre {
+		if e > 0 {
+			ret += e
 		}
-		for d.len() > 0 && e > d.D[d.len()-1] {
-			d.popTail(i)
-		}
-		d.push(e)
-		ret = append(ret, d.max())
 	}
 	return ret
 }
-
-type deque struct {
-	D []int
+func max2(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
-
-func (d *deque) len() int {
-	return len(d.D)
-}
-
-func (d *deque) pop() {
-	d.D = d.D[1:]
-}
-func (d *deque) push(e int) {
-	d.D = append(d.D, e)
-}
-func (d *deque) max() int {
-	return d.D[0]
-}
-func (d *deque) popTail(i int) {
-	d.D = d.D[:len(d.D)-1]
-	//if i == d.len() {
-	//	d.D = []int{}
-	//}
-	//d.D =append(d.D[:i],d.D[i+1:]...)
+func min2(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
