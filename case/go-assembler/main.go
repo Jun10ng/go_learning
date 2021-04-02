2a88bab6724235b07f0a78ca03c79806a0bfa13b
Jun10ng <fu4ng@foxmail.com>
2021-04-02 18:48:50 +0800

    浪费了一天的etcd 和 grpc的坑

--- case/go-assembler/main.go
+++ case/go-assembler/main.go
@@ -3,22 +3,43 @@ package main
 import "fmt"
 
 func main() {
+
 	a := 1
-	b := &arg{
-		C: 2,
-		D: "text",
+
+	//fna(a, nil)
+	b := &fn{
+		D: 2,
+		C: 3,
+		E: nil,
 	}
 	fmt.Println(&b)
-	fna(a, b)
+	fmt.Println(&b.C)
+	fmt.Println(&b.D)
+	f := &arg{
+		13,
+	}
+	fmt.Println(&f.F)
+	b.fnb(a, 10, f)
 }
 
-type arg struct {
+type fn struct {
 	C int
-	D string
+	D int
+	E *arg
+}
+type arg struct {
+	F int
 }
 
-func fna(a int, b *arg) {
+func fna(a int, b *fn) {
 	t := a + b.C
 	fmt.Println(t)
 	fmt.Println(b.D)
+}
+
+func (b *fn) fnb(a int, aa int, cc *arg) {
+	t := a + b.C
+	fmt.Println(t)
+	fmt.Println(b.D)
+	fmt.Println(b.E.F)
 }
