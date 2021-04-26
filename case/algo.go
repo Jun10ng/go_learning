4b453fded74dee00acfc30f6cc06abe9b99eab77
Jun10ng <fu4ng@foxmail.com>
2021-04-26 21:46:00 +0800

    update

--- case/algo.go
+++ case/algo.go
@@ -1,5 +1,54 @@
 package main
 
-func main() {
+import "fmt"
 
+/*
+多数元素
+给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素。
+
+你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+
+
+示例 1：
+
+输入：[3,2,3]
+输出：3
+示例 2：
+
+输入：[2,2,1,1,1,2,2]
+输出：2
+*/
+
+func find(nums []int) (int, bool) {
+	ptr := 0
+	cnt := 1
+	maxCnt := 0
+
+	for i := 1; i < len(nums); i++ {
+		e := nums[i]
+		if e == nums[ptr] {
+			cnt++
+		} else {
+			cnt--
+		}
+		if cnt == 0 {
+			ptr = i + 1
+			cnt = 1
+			i++
+		}
+		maxCnt = max2(cnt, maxCnt)
+	}
+	if cnt == maxCnt {
+		return 0, false
+	}
+	return nums[ptr], true
+}
+func max2(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+func main() {
+	fmt.Println(find([]int{2, 1, 2, 1, 3, 3}))
 }
--- go-source-code-labs/view_gc.go
+++ go-source-code-labs/view_gc.go
@@ -5,8 +5,6 @@ import (
 	"runtime"
 	"runtime/debug"
 	"unsafe"
-	//"runtime"
-	//"unsafe"
 )
 
 func main() {
@@ -14,28 +12,23 @@ func main() {
 }
 
 type myStruct struct {
-	P [1 << 20]int64
+	P [1 << 21]int64
 }
 
 //go:noinline
 func fnc() {
-	mx := &myStruct{}
-	my := &myStruct{}
-	mx.P[0] = 1
+	mx := &myStruct{} // mx -> 0xc00001c080
+	my := &myStruct{} // my -> 0xc00001c090
+	mx.P[0] = 1000
 	my.P[0] = 2
-
 	oldmx := uintptr(unsafe.Pointer(mx))
-
-	// 原本的mx, tx 已经没有被引用了
 	mx = my
-
 	for i := 0; i < 100; i++ {
 		runtime.GC()
 		debug.FreeOSMemory()
 	}
 
 	oldmxStc := (*myStruct)(unsafe.Pointer(oldmx))
-	//oldtxStc := (*myTiny)(unsafe.Pointer(oldtx))
 	fmt.Println(oldmxStc.P[0])
-	//fmt.Println(oldtxStc.P[0])
+	fmt.Println(mx.P[0])
 }
